2f502ca2c4805011048d000a6ca73d466f48c770
Faraz Kaleem Malik <farazmalik17465@gmail.com>
2022-04-08 16:45:12 -0400

    Summer Plans

---
+++ summer_plans.c
@@ -0,0 +1,12 @@
+/* Description:
+*
+* This program was written by Faraz Malik. Copy idk its nothing special
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("This summer, I am planning to take a full CS course load.");
+    return 0;
+}
